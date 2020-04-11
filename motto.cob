4881dd88377a807c6f2c81f86b02b031766b660c
Tim Nigh <nigh.timothy@gmail.com>
2020-04-11 16:58:17 -0400

    tried a couple programs, made minimum and motto

---
+++ motto.cob
@@ -0,0 +1,13 @@
+       identification division.
+           program-id. hello.
+       data division.
+       procedure division.
+
+       program-begin.
+           display "This program contains four DIVISIONS,".
+           display "Three PARAGRAPHS".
+           display "And four SENTENCES".
+       program-done.
+           stop run.
+           
+
