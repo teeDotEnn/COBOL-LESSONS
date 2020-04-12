a8f632f8633231b80ea0e9eab3949d43a9b789a4
Tim Nigh <nigh.timothy@gmail.com>
2020-04-12 19:47:54 -0400

    completed day two

---
+++ dayOne/comment.cob
@@ -0,0 +1,16 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SENTENCES.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+      **************************
+      * This program demonstrates how comments in COBOL work
+      *
+      *
+      ************************* 
+       PROGRAM-BEGIN.      
+           DISPLAY "THIS PROGRAM HAS 4 DIVISIONS".
+           DISPLAY "THREE PARAGRAPHS".
+           DISPLAY "AND FOUR SENTENCES".
+       PROGRAM-END.   
+           STOP RUN.
